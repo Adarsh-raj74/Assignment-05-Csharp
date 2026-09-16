dbe2619326295d8dded7fd5eae56b5e9a97e1b4c
Adarsh rajput <adarshraj7457@gmail.com>
2026-09-16 09:56:42 +0530

    Add files via upload

---
+++ Question4.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Assignment5
+{
+    internal class Question4
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.Write("Enter First Number: ");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter Second Number: ");
+                int b = Convert.ToInt32(Console.ReadLine());
+                int result = a / b;
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Error: please enter numeric value only.");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Error: cannot divided by zero.");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error: some unexpected error occurred");
+            }
+
+        }
+    }
+}
